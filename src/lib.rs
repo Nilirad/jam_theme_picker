73e17d8df700603f27b80f259884be2baa83f057
Nilirad <gisquerin@gmail.com>
2022-02-21 19:01:18 +0100

    Add basic functionality

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,44 @@
 #![doc = include_str!("../README.md")]
 
 use bevy::prelude::*;
-
-pub mod utils;
+use randomorg::Random;
 
 /// A plugin
-pub struct HelloWorldPlugin;
+pub struct ThemePickerPlugin;
 
-impl Plugin for HelloWorldPlugin {
+impl Plugin for ThemePickerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(hello_world.system());
+        app.init_resource::<Themes>()
+            .add_startup_system(setup_themes)
+            .add_system(pick_theme);
     }
 }
 
-fn hello_world() {
-    println!("Hello, World!");
+#[derive(Debug, Default)]
+struct Themes(Vec<String>);
+
+fn setup_themes(mut themes: ResMut<Themes>) {
+    if std::env::args().len() < 2 {
+        panic!("You must provide at least two themes!");
+    }
+
+    themes.0 = std::env::args().collect();
+}
+
+fn pick_theme(themes: Res<Themes>) {
+    // random.org terms of use didn't mention anything about sharing API keys, so...
+    let api_key = include_str!("random-api-key.txt");
+    let rng = Random::new(api_key);
+    let max_index = (themes.0.len() as i32) - 1;
+    let index = rng
+        .generate_integers(1, max_index, 1, true)
+        .expect("Failed to retrieve a random value from random.org.")
+        .result
+        .random
+        .data
+        .first()
+        .copied()
+        .expect("Response from random.org is empty!") as usize;
+
+    println!("The chosen theme is: {}", themes.0[index]);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,6 @@
 use bevy::prelude::*;
-use bevy::window::WindowMode;
-use template_lib::*;
+use theme_picker_lib::*;
 
 fn main() {
-    App::new()
-        // Configure the game window
-        .insert_resource(WindowDescriptor {
-            width: 1920.0,
-            height: 1080.0,
-            title: "Template".to_string(),
-            mode: WindowMode::BorderlessFullscreen,
-            ..Default::default()
-        })
-        // Standard Bevy functionality
-        .add_plugins(DefaultPlugins)
-        // Add plugins here
-        .add_plugin(HelloWorldPlugin)
-        .run();
+    App::new().add_plugin(ThemePickerPlugin).run();
 }
